bd6e821dcceb3645f4891303a608aaa7e1ff3987
devChristiaan <cwhougaard@gmail.com>
2025-12-03 19:01:22 -0500

    added a change listener to scroll to the bottom of the log automatically

--- src/main/java/org/utilities/PopoutLogger.java
+++ src/main/java/org/utilities/PopoutLogger.java
@@ -1,5 +1,8 @@
 package org.utilities;
 
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
@@ -15,6 +18,19 @@ public class PopoutLogger {
         TextArea logDisplay = new TextArea();
         logStreamer = new LogStreamer(logDisplay);
         logDisplay.setEditable(false);
+
+        logDisplay.textProperty().addListener(new ChangeListener<Object>() {
+            @Override
+            public void changed(ObservableValue<?> observable,
+                                Object oldValue,
+                                Object newValue) {
+                // Must run on the JavaFX Application Thread
+                Platform.runLater(() -> {
+                    // Set the vertical scroll position to the maximum value
+                    logDisplay.setScrollTop(Double.MAX_VALUE);
+                });
+            }
+        });
 
         VBox layout = new VBox();
         layout.setPadding(new Insets(14));
